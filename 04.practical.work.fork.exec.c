4c65baac33de0d28dc8e6bd6810693e09b99abc2
Oketola Praise Oluwabamise <oketola.praise@gmail.com>
2019-05-03 17:19:05 +0700

    Labwork 5

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -4,20 +4,20 @@ int main() {
 printf("Main before fork()\n");
 int pid = fork();
 int pid2 = fork();
-if (pid == 0) {
+	if (pid == 0) {
 
-	printf("I am child after fork(), launching ps -ef\n");
-	char *args[]= { "/bin/ps", "-ef" , NULL};
-	execvp("/bin/ps", args);
-	printf("Finished launching ps -ef\n");
+		printf("I am child after fork(), launching ps -ef\n");
+		char *args[]= { "/bin/ps", "-ef" , NULL};
+		execvp("/bin/ps", args);
+		printf("Finished launching ps -ef\n");
 
-}else if(pid2 == 0){
+	}else if(pid2 == 0){
 	
-	printf("I am child after fork(), launching free -h\n");
-	char *args2[] = { "/bin/free", "-h" , NULL};
-	execvp("/bin/free", args2);
-	printf("Finished launching free -h\n");
-}
-else printf("I am parent after fork(), child is %d\n", pid);
+		printf("I am child after fork(), launching free -h\n");
+		char *args2[] = { "/usr/bin/free", "-h" , NULL};
+		execvp("/usr/bin/free", args2);
+		printf("Finished launching free -h\n");
+	}
+	else printf("I am parent after fork(), child is %d\n", pid);
 return 0;
 }
